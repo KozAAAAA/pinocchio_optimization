62fcfd7ec908ca73dcd2a95cb5ff342b31fd9173
Mateusz Koza <mateusz.koza@grinn-global.com>
2024-12-24 15:35:23 +0100

    Finish task 1

--- main.py
+++ main.py
@@ -3,38 +3,62 @@ import numpy as np
 from scipy.interpolate import interp1d
 from environment import Environment
 from robot import Robot
+import pinocchio as pin
+
+TP = 1.0 / 500
+SIM_TIME = 10.0
+URDF_PATH = "iiwa_cup.urdf"
 
 
 def main():
-    Tp = 1.0 / 500
-    end_time = 3.0
-    urdf_path = "iiwa_cup.urdf"
+    robot = Robot(URDF_PATH)
 
-    robot = Robot(urdf_path)
+    desired_ddq = np.array([1.5, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0])
 
-    q = np.array(
-        [
-            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-        ]
-    )
-    t = np.linspace(0.0, end_time, q.shape[0])
+    oMdes = [
+        pin.SE3(np.array([[-0.859547, 0.505643, -0.0741905], [-0.505646, -0.820367, 0.267059], [0.0741733, 0.267064, 0.96082]]), np.array([0.468445, 0.236337, 0.934516])),
+        pin.SE3(np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]), np.array([0.0, 0.0, 1.261])),
+        pin.SE3(np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]), np.array([0.5, 0.5, 0.5])),
+        pin.SE3(np.array([[1, 0, 0], [0, 0, 1], [1, 0, 0]]), np.array([0.3, 0.3, 0.3])),
+    ]
+    
+    q = np.zeros(robot.model.nq)
+    for i, oMd in enumerate(oMdes):
+        sucess, qd, err = robot.inverse_kinematics(oMd, "F_joint_7")
+        if sucess:
+            print(f"oMdes[{i}] solved")
+            qd = [(angle + np.pi) % (2 * np.pi) - np.pi for angle in qd]
+            q = np.vstack((q, qd))
+        else:
+            print(f"oMdes[{i}] not solved")
+            return
 
+    t = np.linspace(0.0, SIM_TIME, q.shape[0])
     qi = interp1d(t, q, axis=0)
 
-    env = Environment(urdf_path, Tp, q[0], [0.5, 0.5, 0.5], 0.15)
+    env = Environment(
+        urdf_path=URDF_PATH,
+        timestep=TP,
+        q0l=q[0],  # Initial joint positions
+        xyz=[0.5, 0.5, 0.5],  # Generate sphere at xyz
+        r=0.15,  # Radius of the sphere
+    )
 
-    for i in range(int(end_time / Tp)):
-        t_act = i * Tp
-
+    for i in range(int(SIM_TIME / TP)):
+        print(robot.data.oMi[-1])
+        t_act = i * TP
         q_act, dq_act = env.get_state()
-        robot.forward_kinematics(q_act)
+
+        M = robot.forward_kinematics(q_act)
+        tau = robot.inverse_dynamics(q_act, dq_act, desired_ddq)
+        ddq = robot.forward_dynamics(q_act, dq_act, tau)
+        J = robot.jacobian(q_act, "F_joint_7")
 
         q_i = qi(t_act)
         env.reset_joints_state(q_i, np.zeros_like(q_i))
         env.simulation_step()
-        sleep(Tp)
+        sleep(TP)
+
 
 if __name__ == "__main__":
     main()
--- robot.py
+++ robot.py
@@ -1,27 +1,101 @@
 import pinocchio as pin
+import numpy as np
+from numpy.linalg import norm, solve
 
-class Robot():
+class Robot:
     def __init__(self, urdf_path):
+        """
+        self.data - structure used to store non constant data
+        self.data.oMi:
+            - o - origin
+            - M - transformation matrix
+            - i - ith link
+        """
+
         self.model = pin.buildModelFromUrdf(urdf_path)
-        # Data needed to store non constant data
         self.data = self.model.createData()
 
     def forward_kinematics(self, q):
+        """Find the transformation matrix of the end effector given the joint positions"""
         pin.forwardKinematics(self.model, self.data, q)
-        for name, oMi in zip(self.model.names, self.data.oMi):
-            print("{:<24} : {: .2f} {: .2f} {: .2f}".format(name, *oMi.translation.T.flat))
-        
+        pin.updateFramePlacements(self.model, self.data)
+        return self.data.oMi[-1]
 
-    def inverse_kinematics(self, x):
-        '''has to be efficient'''
-        pass
+    def inverse_kinematics(
+        self,
+        oMdes,
+        joint_name,
+        eps=1e-4,
+        it_max=1000,
+        dt=1e-1,
+        damp=1e-12,
+    ):
+        """Find the joint positions given the transformation matrix of the end effector"""
+        q = pin.neutral(self.model)
+        joint_id = self.model.getJointId(joint_name)
+        i = 0
+        while True:
+            pin.forwardKinematics(self.model, self.data, q)
+            dMi = oMdes.actInv(self.data.oMi[joint_id])
+            err = pin.log(dMi).vector
+            if norm(err) < eps:
+                success = True
+                break
+            if i >= it_max:
+                success = False
+                break
+            J = pin.computeJointJacobian(self.model, self.data, q, joint_id)
+            v = -J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
+            q = pin.integrate(self.model, q, v * dt)
+            i += 1
+
+        return success, q, err
 
     def forward_dynamics(self, q, dq, tau):
-        pass
+        """
+        Find accelerations given the joint positions, velocities and torques
+        """
+        ddq = pin.aba(self.model, self.data, q, dq, tau)
+        return ddq
 
     def inverse_dynamics(self, q, dq, ddq):
-        pass
+        """
+        Find torques given the joint positions, velocities and accelerations
+        q - current joint positions
+        dq - current joint velocities
+
+        ddq - desired joint accelerations
+        """
+        tau = pin.rnea(self.model, self.data, q, dq, ddq)
+        return tau
+
+    def jacobian(self, q, frame_name):
+        """
+        Calculates the Jacobian matrix of size 6 x n, where n is the number of joints (DOF).
+        The number 6 comes from 3 linear velocities and 3 angular velocities.
+
+        dX = [dx, dy, dz, droll, dpitch, dyaw] - end effector velocities
+        dq = [dq1, dq2, dq3, dq4, dq5, dq6, dq7] - joint velocities
 
 
-        pass
+               l1   l2   l3   l4   l5   l6   l7
 
+        J = | J11  J12  J13  J14  J15  J16  J17 |   dx
+            | J21  J22  J23  J24  J25  J26  J27 |   dy
+            | J31  J32  J33  J34  J35  J36  J37 |   dz
+            | J41  J42  J43  J44  J45  J46  J47 |   droll
+            | J51  J52  J53  J54  J55  J56  J57 |   dpitch
+            | J61  J62  J63  J64  J65  J66  J67 |   dyaw
+
+        Jij - how j-th joint velocity affects i-th end effector velocity
+
+        dX = J * dq
+        """
+        J = pin.computeFrameJacobian(
+            model=self.model,
+            data=self.data,
+            q=q,
+            frame_id=self.model.getFrameId(frame_name),
+            reference_frame=pin.ReferenceFrame.LOCAL_WORLD_ALIGNED,
+        )
+        return J
